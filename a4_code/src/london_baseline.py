0205b65b8c138ec87513869caf5c1a812dd13146
rnluo <ning.l.rn@connect.hku.hk>
2025-08-03 13:41:20 +0800

    upload

---
+++ a4_code/src/london_baseline.py
@@ -0,0 +1,26 @@
+# TODO: [part d]
+# Calculate the accuracy of a baseline that simply predicts "London" for every
+#   example in the dev set.
+# Hint: Make use of existing code.
+# Your solution here should only be a few lines.
+
+import argparse
+import utils
+
+def main():
+    accuracy = 0.0
+
+    # Compute accuracy in the range [0.0, 100.0]
+    ### YOUR CODE HERE ###
+
+    with open('birth_dev.tsv', encoding='utf-8') as fin:
+        lines = [x.strip().split('\t') for x in fin]
+    accuracy = len(list(filter(lambda x: x[1] == 'London', lines))) / len(lines) * 100
+
+    ### END YOUR CODE ###
+    return accuracy
+
+if __name__ == '__main__':
+    accuracy = main()
+    with open("london_baseline_accuracy.txt", "w", encoding="utf-8") as f:
+        f.write(f"{accuracy}\n")
